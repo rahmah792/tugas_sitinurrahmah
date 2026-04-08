e03fe3351fa83c0f2d50de57a2039f0bed34e327
rahmah792 <sitastinrhmhwhyymaa@gmail.com>
2026-04-09 05:18:35 +0700

    Add files via upload

---
+++ tugaspemrogramanII/src/tugas/aplikasi_kasir.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas;
+
+/**
+ *
+ * @author User
+ */
+public class aplikasi_kasir {
+    
+    /**
+     * @param args the command line argumens 
+     */
+    public static void main(String[] args){
+        new form_utama().show();
+        //TODO code aplication logic here
+    }
+}
